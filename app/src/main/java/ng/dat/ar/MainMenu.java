ba5c60a8703ecba18cf90afc9efd8d0e837a98e7
David Dryburgh <david.dryburgh1994@gmail.com>
2018-03-28 14:06:21 +0100

    Initial Commit
    Initial Commit

---
+++ app/src/main/java/ng/dat/ar/MainMenu.java
@@ -0,0 +1,41 @@
+package ng.dat.ar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenu extends AppCompatActivity
+{
+    Button explore, tours, preferences, help;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+        explore = (Button) findViewById(R.id.btn_explore);
+    }
+
+    public void launchExplore(View view)
+    {
+        startActivity(new Intent(this, ARActivity.class));
+    }
+
+    public void launchTours(View view)
+    {
+        startActivity(new Intent(this, ToursActivity.class));
+    }
+
+    public void launchPreferences(View view)
+    {
+        startActivity(new Intent(this, PreferencesActivity.class));
+    }
+
+    public void launchHelp(View view)
+    {
+        startActivity(new Intent(this, HelpActivity.class));
+    }
+}
